fa91e369e1e93731bf5c8958bc1ad78b45adb079
omar84-html <abdoboy666@hotmail.com>
2026-04-08 12:42:18 +0200

    hovedside takeaway

---
+++ scripts/views/viewHovedside.js
@@ -0,0 +1,28 @@
+updateView()
+function updateView() {
+    app.innerHTML = /*HTML*/`
+        <div class="header">
+            <h1>Takeaway Larvik</h1>
+            <input placeholder="søk etter restauranter, retter, hovedingredienser">
+        </div>
+        <div class="seksjon">
+            <h2>Dine måltider:</h2>
+            <p>Det du bestiller oftest</p>
+            <div id="maltider"></div>
+        </div>
+        <div class="seksjon">
+            <h2>Dagens:</h2>
+            <p>Tilbud og nyheter fra forskjellige restauranter i Larvik</p>
+            <div id="dagens"></div>
+        </div>
+        <div class="bunn-nav">
+            <button>Filter</button>
+            <button>Handlekurv</button>
+            <button>Konto</button>
+        </div>
+    `
+    visProduktMaltider()
+    visDagens()
+}
+
+updateView()
