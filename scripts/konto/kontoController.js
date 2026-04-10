4abde70d8a4096c3c1e5e8d37af4ee742a544455
omar84-html <abdoboy666@hotmail.com>
2026-04-10 14:50:28 +0200

    kkkk

--- scripts/konto/kontoController.js
+++ scripts/konto/kontoController.js
@@ -2,3 +2,41 @@ function loggUt() {
     modell.app.logInId = null;
     EndreView('Home');
 }
+
+function visLoggInn() {
+    document.getElementById("main").innerHTML = loggInnView();
+}
+
+function loggInn() {
+    const bruker = modell.data.user.find(u => 
+        u.email === modell.viewstate.logIn.username && 
+        u.password === modell.viewstate.logIn.password
+    );
+
+    if (bruker) {
+        modell.app.logInId = bruker.id;
+        visKonto();
+    } else {
+        alert("Feil email eller passord!");
+    }
+}
+
+function visRegistrer() {
+    document.getElementById("main").innerHTML = registrerView();
+}
+
+function registrerBruker() {
+    const ny = modell.viewstate.createUser;
+    const bruker = {
+        id: modell.data.user.length + 1,
+        name: ny.name,
+        email: ny.email,
+        phoneNr: ny.phoneNr,
+        password: ny.password,
+        hasAdmin: false,
+        allergiesId: []
+    };
+    modell.data.user.push(bruker);
+    modell.app.logInId = bruker.id;
+    visKonto();
+}
